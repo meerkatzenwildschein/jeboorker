2e823918cba83620c05cddb9c537108e0983a540
meerkatzenwildschein <guru@freya>
2015-04-05 18:32:04 +0200

    bad perfromance testing code removed

--- commons/src/test/java/org/rr/commons/utils/ListUtilsTest.java
+++ commons/src/test/java/org/rr/commons/utils/ListUtilsTest.java
@@ -10,17 +10,17 @@ public class ListUtilsTest extends TestCase {
 	public void testSplit() {
 		List<String> split = ListUtils.split("1,2,3", ",", -1, UtilConstants.COMPARE_BINARY);
 		assertEquals(3, split.size());
-		
+
 		split = ListUtils.split("1,2,3", ",", 1, UtilConstants.COMPARE_BINARY);
-		assertEquals(1, split.size());		
-		
+		assertEquals(1, split.size());
+
 		split = ListUtils.split("1,2,3", ",", 3, UtilConstants.COMPARE_BINARY);
-		assertEquals(3, split.size());	
-		
+		assertEquals(3, split.size());
+
 		split = ListUtils.split("1,2,3", ",", 0, UtilConstants.COMPARE_BINARY);
-		assertEquals(0, split.size());				
+		assertEquals(0, split.size());
 	}
-	
+
 	public void testChunkSplit() {
 		List<String> chunkSplit = ListUtils.chunkSplit("teststring", 3); //[tes, tst, rin, g  ]
 		assertEquals(4, chunkSplit.size());
@@ -29,67 +29,46 @@ public class ListUtilsTest extends TestCase {
 		assertEquals("rin", chunkSplit.get(2));
 		assertEquals("g  ", chunkSplit.get(3));
 	}
-	
+
 	public void testSet() {
 		ArrayList<String> l = new ArrayList<>();
 		l.add("1");
-		
+
 		ListUtils.set(l, "1new", 0);
-		assertEquals("1new", l.get(0));	
-		
+		assertEquals("1new", l.get(0));
+
 		ListUtils.set(l, "farawy", 5);
 		assertEquals("farawy", l.get(5));
-		
+
 		ListUtils.set(l, "farawy2", 6);
 		assertEquals("farawy2", l.get(6));
-		
+
 		ListUtils.set(l, "farawy3", 6);
-		assertEquals("farawy3", l.get(6));		
-		
+		assertEquals("farawy3", l.get(6));
+
 	}
-	
+
 	public void testSplitChar() {
 		List<String> split = ListUtils.split("aay,b,c,,,", ',');
 		assertEquals("[aay, b, c, , , ]", split.toString());
-		
+
 		split = ListUtils.split("", ',');
 		assertEquals(1, split.size());
-		
+
 	}
-	
+
 	public void testIndexedToString() {
-		long currentTimeMillis;
 		ArrayList<String> list = new ArrayList<>();
 		for(int i = 0; i < 100; i++) {
 			list.add("/home/user/ebooks/test/folder name with spaces/some book with a name.epub" + i);
 		}
-		
-		currentTimeMillis = System.currentTimeMillis();
-		String indexedListString = ListUtils.toIndexedString(list, ';');
-		long indexedToStringTime = System.currentTimeMillis() - currentTimeMillis;
-		System.out.println("index toString: " + indexedToStringTime);
-		
-		currentTimeMillis = System.currentTimeMillis();
-		String oldFashionListString = ListUtils.join(list, ";");
-		long oldToStringTime = System.currentTimeMillis() - currentTimeMillis;
-		System.out.println("old toString: "+oldToStringTime);
 
-		
-		currentTimeMillis = System.currentTimeMillis();
+		String indexedListString = ListUtils.toIndexedString(list, ';');
+		String oldFashionListString = ListUtils.join(list, ";");
 		List<String> fromIndexString = ListUtils.fromIndexString(indexedListString, ';');
-		long indexedTime = System.currentTimeMillis() - currentTimeMillis;
-		System.out.println("indexed toList: "+indexedTime);
-		
-		currentTimeMillis = System.currentTimeMillis();
 		List<String> split = ListUtils.split(oldFashionListString, ";");
-		long oldFashionTime = System.currentTimeMillis() - currentTimeMillis;
-		System.out.println("old toList: " + oldFashionTime);
-		
+
 		assertEquals(list.size(), fromIndexString.size());
 		assertEquals(split.size(), fromIndexString.size());
-		
-		//performance
-		assertTrue(indexedTime < oldFashionTime);
-		
-	}	
+	}
 }
