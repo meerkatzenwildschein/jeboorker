1a10415752e37a1992d961979231c538735db5f4
meerkatzenwildschein <guru@freya>
2016-05-24 21:02:10 +0200

    remove old java 1.7 priority selection from linux start script.

--- dist/jeboorker.sh
+++ dist/jeboorker.sh
@@ -4,28 +4,6 @@ cd `dirname $(readlink -f $0)`
 JAVABIN="/usr/bin/java";
 CLASSPATH=$(echo lib/*.jar | tr ' ' ':')
 
-if [ -f /usr/bin/update-alternatives ]
-then
-for entry in `update-alternatives --list java`; do 
-
-	case "$entry" in
-		*"java-7"* )
-		    JAVABIN="$entry"
-			break;
-		    ;;
-		*)
-	esac
-	case "$entry" in
-		*"jdk1.7"* )
-		    JAVABIN="$entry"
-			break;
-		    ;;
-		*)
-	esac
-
-done
-fi
-
 echo using java binary $JAVABIN
 echo starting with classpath $CLASSPATH
 $JAVABIN -splash:splashscreen.gif -client -Xmx512m -XX:+UseParNewGC -XX:MaxHeapFreeRatio=20 -XX:MinHeapFreeRatio=10 -XX:+AggressiveOpts $_JCONSOLE -cp $CLASSPATH org.rr.jeborker.Jeboorker $@
