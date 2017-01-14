7f9181f33410a9fc2d15f48e969e8b626d0d4c49
rrauschenbach <guru@freya>
2017-01-14 17:54:41 +0100

    remove unused method

--- commons/src/main/java/org/rr/commons/utils/NumberUtil.java
+++ commons/src/main/java/org/rr/commons/utils/NumberUtil.java
@@ -1,14 +1,8 @@
 package org.rr.commons.utils;
 
-import java.nio.ByteBuffer;
-
 public class NumberUtil {
 
 	private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
-
-	public static byte[] toByteArray(long l) {
-		return ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(l).array();
-	}
 
 	public static String bytesToHex(byte[] bytes) {
 		char[] hexChars = new char[bytes.length * 2];
