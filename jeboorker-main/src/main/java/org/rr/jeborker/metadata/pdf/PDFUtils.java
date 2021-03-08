ef09616a5ba81f50b7bc5db1387d95d2e93654aa
rrauschenbach <guru@freya>
2021-03-08 21:05:48 +0100

    Make PdfReader only use simple InputStream instead of FileChannelRandomAccessSource

--- jeboorker-main/src/main/java/org/rr/jeborker/metadata/pdf/PDFUtils.java
+++ jeboorker-main/src/main/java/org/rr/jeborker/metadata/pdf/PDFUtils.java
@@ -4,27 +4,21 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.RandomAccessFile;
-import java.nio.channels.FileChannel;
 import java.util.List;
 import java.util.logging.Level;
 
 import org.rr.commons.log.LoggerFactory;
 import org.rr.commons.mufs.IResourceHandler;
-import org.rr.jeborker.app.preferences.APreferenceStore;
-import org.rr.jeborker.app.preferences.PreferenceStoreFactory;
 import org.rr.jeborker.converter.ConverterUtils;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.io.FileChannelRandomAccessSource;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfImportedPage;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.pdf.RandomAccessFileOrArray;
 
 public class PDFUtils {
 
@@ -37,63 +31,7 @@ public class PDFUtils {
 	 * @throws IOException
 	 */
 	public static PdfReader getReader(File pdfFile) throws IOException {
-		if (isLargePdfFile(pdfFile)) {
-			return new InputStreamPDFReaderDelegate(new BufferedInputStream(new FileInputStream(pdfFile)), pdfFile);
-		}
-
-		// more performant but needs memory in one piece 
-		RandomAccessFile file = new RandomAccessFile(pdfFile, "r");
-		FileChannel fileChannelI = file.getChannel();
-		FileChannelRandomAccessSource fileChannelRandomAccessSource = new FileChannelRandomAccessSource(fileChannelI);
-		RandomAccessFileOrArray rafPdfIn = new RandomAccessFileOrArray(fileChannelRandomAccessSource);
-		return new RandomAccessFilePDFReaderDelegate(rafPdfIn, file, fileChannelI);
-	}
-
-	private static boolean isLargePdfFile(File pdfFile) {
-		APreferenceStore preferenceStore = PreferenceStoreFactory.getPreferenceStore(PreferenceStoreFactory.SYSTEM_STORE);
-		int thresholdInMb = preferenceStore.getGenericEntryAsNumber("pdf.random_access.threshold.mb", Integer.valueOf(100)).intValue();
-		long fileSizeInMb = pdfFile.length() / 1024 / 1024;
-		boolean isLargePdfFile = fileSizeInMb > thresholdInMb;
-		return isLargePdfFile;
-	}
-
-	private static class RandomAccessFilePDFReaderDelegate extends PdfReader {
-
-		private FileChannel fileChannelI;
-
-		private RandomAccessFile file;
-
-		RandomAccessFilePDFReaderDelegate(RandomAccessFileOrArray rafPdfIn, RandomAccessFile file, FileChannel fileChannelI)
-				throws IOException {
-			super(rafPdfIn, null);
-			this.file = file;
-			this.fileChannelI = fileChannelI;
-		}
-
-		RandomAccessFilePDFReaderDelegate(InputStream in) throws IOException {
-			super(in, null);
-		}
-
-		@Override
-		public void close() {
-			super.close();
-			try {
-				if (file != null) {
-					file.close();
-				}
-			} catch (IOException e) {
-				LoggerFactory.getLogger(this).log(Level.WARNING, "Failed to close File " + file, e);
-			}
-
-			if (fileChannelI != null) {
-				try {
-					fileChannelI.close();
-					fileChannelI = null;
-				} catch (IOException e) {
-					LoggerFactory.getLogger(this).log(Level.WARNING, "Failed to close File " + file, e);
-				}
-			}
-		}
+		return new InputStreamPDFReaderDelegate(new BufferedInputStream(new FileInputStream(pdfFile)), pdfFile);
 	}
 
 	private static class InputStreamPDFReaderDelegate extends PdfReader {
