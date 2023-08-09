e5a53a9b7366965b087e544690a06de8d388adce
Rüdiger Rauschenbach <Ruediger.Rauschenbach@kv.digital>
2023-08-09 15:47:08 +0200

    Fix web image scraper

--- commons/src/main/java/org/rr/commons/net/imagefetcher/BigBookSearchImageFetcher.java
+++ commons/src/main/java/org/rr/commons/net/imagefetcher/BigBookSearchImageFetcher.java
@@ -13,130 +13,161 @@ import java.util.logging.Level;
 import org.rr.commons.log.LoggerFactory;
 import org.rr.commons.mufs.IResourceHandler;
 import org.rr.commons.mufs.ResourceHandlerFactory;
-import org.rr.commons.utils.CommonUtils;
-import org.rr.commons.utils.HTMLEntityConverter;
 
-class BigBookSearchImageFetcher extends AImageFetcher {
-	
-	private int page = 0;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-	/**
-	 * Perform a google image search and returns the result.
-	 * @param searchTerm The search phrase for the search.
-	 * @param page The page number starting with 1.
-	 *
-	 * @return All images found by the search.
-	 * @throws IOException
-	 *
-	 * @see https://developers.google.com/image-search/v1/jsondevguide#json_snippets_java
-	 */
-	private  List<IImageFetcherEntry> searchImages(String searchTerm, int page) throws IOException {
-		final String encodesSearchPhrase = URLEncoder.encode(searchTerm, "UTF-8");
-		
-		//http://bigbooksearch.com/query.php?SearchIndex=books&Keywords=katze+tod&ItemPage=1
-		String urlString = "http://bigbooksearch.com/query.php?SearchIndex=books&Keywords=" + encodesSearchPhrase + "&ItemPage=" + page;
-		LoggerFactory.getLogger(this).log(Level.INFO, "Loading... " + urlString);
-		final IResourceHandler resourceLoader = ResourceHandlerFactory.getResourceHandler(urlString);
-		try {
-			final byte[] content = resourceLoader.getContent();
-			final String contentString = new String(content);
-			if(contentString.indexOf("<img id='item-") != -1) {
-				final String[] splited = contentString.split("<img id='item-");
-				final ArrayList<IImageFetcherEntry> result = new ArrayList<>(splited.length-1);
-				for (int i = 1; i < splited.length; i++) {
-					IImageFetcherEntry image = new BingImageFetcherEntry(splited[i]);
-					result.add(image);
-				}
-				return result;
-			}
-		} finally {
-			resourceLoader.dispose();
-		}
-		
-		return Collections.emptyList();
-	}
+class BigBookSearchImageFetcher extends AImageFetcher
+{
 
-	public List<IImageFetcherEntry> getNextEntries() throws IOException {
-		if(getSearchTerm() == null || getSearchTerm().isEmpty()) {
-			return Collections.emptyList();
-		}
-		return searchImages(getSearchTerm(), ++page);
-	}
-	
-	/**
-	 * The implementation for one image search result entry.
-	 */
-	private static class BingImageFetcherEntry extends AImageFetcherEntry {
-		
-		private String htmlSearchEntry;
-		
-		BingImageFetcherEntry(String htmlSearchEntry) {
-			this.htmlSearchEntry = htmlSearchEntry;
-		}
+    /**
+     * Perform a google image search and returns the result.
+     *
+     * @param searchTerm The search phrase for the search.
+     * @return All images found by the search.
+     * @throws IOException
+     * @see https://developers.google.com/image-search/v1/jsondevguide#json_snippets_java
+     */
+    private List<IImageFetcherEntry> searchImages(String searchTerm) throws IOException
+    {
+        final String encodesSearchPhrase = URLEncoder.encode(searchTerm, "UTF-8");
 
-		@Override
-		public URL getThumbnailURL() {
-			try {
-				return getImageURL();
-			} catch (Exception e) {
-				return null;
-			}
-		}
+        String urlString =
+            "http://bigbooksearch.com/STOP!please-dont-scrape-my-site-you-will-put-my-api-key-over-the-usage-limit-and-the-site-will-break!AGAIN/books/"
+                + encodesSearchPhrase;
+        LoggerFactory.getLogger(this).log(Level.INFO, "Loading... " + urlString);
+        final IResourceHandler resourceLoader = ResourceHandlerFactory.getResourceHandler(urlString);
 
-		@Override
-		public URL getImageURL() {
-			try {
-				String imageSourceURL = getParameterValue(htmlSearchEntry, "src");
-				return new URL(imageSourceURL);
-			} catch (Exception e) {
-				return null;
-			}
-		}
+        try
+        {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(resourceLoader.getContent());
 
-		@Override
-		public int getImageWidth() {
-			try {
-				String imageWidth = getParameterValue(htmlSearchEntry, "width");
-				return CommonUtils.toNumber(imageWidth).intValue();
-			} catch (Exception e) {
-				return 0;
-			}
-		}
+            JsonNode resultsNode = rootNode.get("results");
+            if (resultsNode.isArray())
+            {
+                final ArrayList<IImageFetcherEntry> result = new ArrayList<>();
 
-		@Override
-		public int getImageHeight() {
-			try {
-				String imageHeight = getParameterValue(htmlSearchEntry, "height");
-				return CommonUtils.toNumber(imageHeight).intValue();
-			} catch (Exception e) {
-				return 0;
-			}
-		}
+                for (JsonNode resultNode : resultsNode)
+                {
+                    String link = resultNode.get("link").asText();
+                    String title = resultNode.get("title").asText();
+                    String img = resultNode.get("image").asText();
+                    int width = resultNode.get("width").asInt();
+                    int height = resultNode.get("height").asInt();
 
-		@Override
-		public String getTitle() {
-			try {
-				String imageTitle = getParameterValue(htmlSearchEntry, "alt");
-				HTMLEntityConverter converter = new HTMLEntityConverter(imageTitle, HTMLEntityConverter.ENCODE_EIGHT_BIT_ASCII);
-				imageTitle = converter.decodeEntities();
-				return imageTitle;
-			} catch (Exception e) {
-				return EMPTY;
-			}
-		}
-		
-		/**
-		 * get the first parameter from the given html.
-		 * @param html HTML string to be searched.
-		 * @param parameter Parameter where the value should be returned for.
-		 * @return The value or <code>null</code> if no parameter could be found.
-		 */
-		private String getParameterValue(String html, String parameter) {
-			int parameterStartIndex = html.indexOf(parameter + "='") + parameter.length() + 2;
-			int parameterEndIndex = html.indexOf("'", parameterStartIndex);
-			String value = html.substring(parameterStartIndex, parameterEndIndex);
-			return value;
-		}
-	}
+                    IImageFetcherEntry image = new BigBookImageFetcherEntry(link, img, width, height, title);
+                    result.add(image);
+                }
+                return result;
+            }
+        }
+        catch (Exception e)
+        {
+            LoggerFactory.getLogger(this).log(Level.WARNING, "Failed loading... " + urlString, e);
+        }
+        finally
+        {
+            resourceLoader.dispose();
+        }
+
+        return Collections.emptyList();
+    }
+
+    public List<IImageFetcherEntry> getNextEntries() throws IOException
+    {
+        if (getSearchTerm() == null || getSearchTerm().isEmpty())
+        {
+            return Collections.emptyList();
+        }
+        return searchImages(getSearchTerm());
+    }
+
+    /**
+     * The implementation for one image search result entry.
+     */
+    private static class BigBookImageFetcherEntry extends AImageFetcherEntry
+    {
+
+        private final String link;
+        private final String image;
+        private final int width;
+        private final int height;
+        private final String title;
+
+        private BigBookImageFetcherEntry(String link, String image, int width, int height, String title)
+        {
+            this.link = link;
+            this.image = image;
+            this.width = width;
+            this.height = height;
+            this.title = title;
+        }
+
+        @Override
+        public URL getThumbnailURL()
+        {
+            try
+            {
+                return getImageURL();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        @Override
+        public URL getImageURL()
+        {
+            try
+            {
+                return new URL(image);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        @Override
+        public int getImageWidth()
+        {
+            try
+            {
+                return width;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+
+        @Override
+        public int getImageHeight()
+        {
+            try
+            {
+                return height;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+
+        @Override
+        public String getTitle()
+        {
+            try
+            {
+                return title;
+            }
+            catch (Exception e)
+            {
+                return EMPTY;
+            }
+        }
+    }
 
 }
