7eef50441126d2bd94ec60e40d22439039649271
Rüdiger Rauschenbach <Ruediger.Rauschenbach@kv.digital>
2023-08-09 16:13:56 +0200

    add-opens java.desktop/javax.swing

--- dist/Jeboorker.vbs
+++ dist/Jeboorker.vbs
@@ -2,7 +2,7 @@ Option Explicit
 
 Dim strArguments, wshShell
 
-strArguments = "javaw -splash:splashscreen.gif -client -Xmx512m -XX:MaxHeapFreeRatio=20 -XX:MinHeapFreeRatio=10 -Djava.library.path=.\lib\ --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED -cp .\lib\* org.rr.jeborker.Jeboorker"
+strArguments = "javaw -splash:splashscreen.gif -client -Xmx512m -XX:MaxHeapFreeRatio=20 -XX:MinHeapFreeRatio=10 -Djava.library.path=.\lib\ --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED --add-opens=java.desktop/javax.swing=ALL-UNNAMED -cp .\lib\* org.rr.jeborker.Jeboorker"
 
 Set wshShell = CreateObject( "WScript.Shell" )
 wshShell.Run Trim( strArguments ), 0, False
--- dist/jeboorker.sh
+++ dist/jeboorker.sh
@@ -6,4 +6,4 @@ CLASSPATH=$(echo lib/*.jar | tr ' ' '\n' | sort -g | tr '\n' ':')
 
 echo using java binary $JAVABIN
 echo starting with classpath $CLASSPATH
-$JAVABIN -splash:splashscreen.gif -client -Xmx512m -XX:MaxHeapFreeRatio=20 -XX:MinHeapFreeRatio=10 -Djava.library.path=./lib/ --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED $_JCONSOLE -cp $CLASSPATH org.rr.jeborker.Jeboorker $@
+$JAVABIN -splash:splashscreen.gif -client -Xmx512m -XX:MaxHeapFreeRatio=20 -XX:MinHeapFreeRatio=10 -Djava.library.path=./lib/ --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED --add-opens=java.desktop/javax.swing=ALL-UNNAMED $_JCONSOLE -cp $CLASSPATH org.rr.jeborker.Jeboorker $@
