eef4a98289ecda203196377a56ade65ff05db079
Rüdiger Rauschenbach <Ruediger.Rauschenbach@kv.digital>
2023-08-25 14:03:24 +0200

    Fix version display

--- jeboorker-main/src/main/java/org/rr/jeborker/Jeboorker.java
+++ jeboorker-main/src/main/java/org/rr/jeborker/Jeboorker.java
@@ -1,26 +1,8 @@
 package org.rr.jeborker;
 
-import static org.rr.commons.utils.StringUtil.EMPTY;
-
-import java.awt.EventQueue;
-import java.awt.Frame;
-import java.awt.SplashScreen;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.logging.Level;
-
+import it.sauronsoftware.junique.AlreadyLockedException;
+import it.sauronsoftware.junique.JUnique;
+import it.sauronsoftware.junique.MessageHandler;
 import org.rr.commons.log.LoggerFactory;
 import org.rr.commons.utils.ListUtils;
 import org.rr.commons.utils.ReflectionUtils;
@@ -29,155 +11,172 @@ import org.rr.commons.utils.compression.rar.RarUtils;
 import org.rr.jeborker.app.JeboorkerLogger;
 import org.rr.jeborker.gui.MainController;
 
-import it.sauronsoftware.junique.AlreadyLockedException;
-import it.sauronsoftware.junique.JUnique;
-import it.sauronsoftware.junique.MessageHandler;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+
+import static org.rr.commons.utils.StringUtil.EMPTY;
 
 public class Jeboorker {
 
-	public static final ExecutorService APPLICATION_THREAD_POOL = new ThreadPoolExecutor(0, 1024,
-			60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), new ApplicationThreadFactory()) {};
+    public static final ExecutorService APPLICATION_THREAD_POOL = new ThreadPoolExecutor(0, 1024,
+            60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), new ApplicationThreadFactory()) {
+    };
 
-	public static final String APP = "Jeboorker";
+    public static final String APP = "Jeboorker";
 
-	public static final String URL = "https://github.com/meerkatzenwildschein/jeboorker";
+    public static final String URL = "https://github.com/meerkatzenwildschein/jeboorker";
 
-	private static MainController mainController;
+    private static MainController mainController;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		//setup the logger
-		LoggerFactory.addHandler(new JeboorkerLogger());
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        //setup the logger
+        LoggerFactory.addHandler(new JeboorkerLogger());
 
-		//Setup the location for the rar executables.
-		if(ReflectionUtils.getOS() == ReflectionUtils.OS_WINDOWS) {
-			RarUtils.setRarExecFolder(getAppFolder() + File.separator + "exec");
-		} else {
-			RarUtils.setRarExecFolder("/usr/bin");
-		}
+        //Setup the location for the rar executables.
+        if (ReflectionUtils.getOS() == ReflectionUtils.OS_WINDOWS) {
+            RarUtils.setRarExecFolder(getAppFolder() + File.separator + "exec");
+        } else {
+            RarUtils.setRarExecFolder("/usr/bin");
+        }
 
-		boolean isRunning = handleAlreadyRunning();
+        boolean isRunning = handleAlreadyRunning();
 
-		if(!isRunning) {
-			try {
-				logSystemInfo();
+        if (!isRunning) {
+            try {
+                logSystemInfo();
 
-				mainController = MainController.getController();
-			} catch (Exception e) {
-				LoggerFactory.log(Level.SEVERE, null, "Main failed", e);
-				System.exit(-1);
-			}
-		} else {
-			// Sends arguments to the already active instance.
-			JUnique.sendMessage(Jeboorker.class.getName(), EMPTY);
+                mainController = MainController.getController();
+            } catch (Exception e) {
+                LoggerFactory.log(Level.SEVERE, null, "Main failed", e);
+                System.exit(-1);
+            }
+        } else {
+            // Sends arguments to the already active instance.
+            JUnique.sendMessage(Jeboorker.class.getName(), EMPTY);
 
-			LoggerFactory.log(Level.INFO, Jeboorker.class, "Jeboorker " + getAppVersion() + " is already running.");
-		}
-		closeSplashScreen();
-	}
+            LoggerFactory.log(Level.INFO, Jeboorker.class, "Jeboorker " + getAppVersion() + " is already running.");
+        }
+        closeSplashScreen();
+    }
 
-	private static boolean handleAlreadyRunning() {
-		try {
-			JUnique.acquireLock(Jeboorker.class.getName(), new MessageHandler() {
-				public String handle(String message) {
-					if (mainController != null) {
-						EventQueue.invokeLater(new Runnable() {
-						    @Override
-						    public void run() {
-					        int state = mainController.getMainWindow().getExtendedState();
-					        state &= ~Frame.ICONIFIED;
+    private static boolean handleAlreadyRunning() {
+        try {
+            JUnique.acquireLock(Jeboorker.class.getName(), new MessageHandler() {
+                public String handle(String message) {
+                    if (mainController != null) {
+                        EventQueue.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                int state = mainController.getMainWindow().getExtendedState();
+                                state &= ~Frame.ICONIFIED;
 
-					        mainController.getMainWindow().setExtendedState(state);
-					        mainController.getMainWindow().setFocusableWindowState(false);
-						    	mainController.getMainWindow().toFront();
-						    	mainController.getMainWindow().setFocusableWindowState(true);
-						    }
-						});
-					}
-					return null;
-				}
-			});
-			return false;
-		} catch (AlreadyLockedException e) {
-			// Application already running.
-			return true;
-		}
-	}
+                                mainController.getMainWindow().setExtendedState(state);
+                                mainController.getMainWindow().setFocusableWindowState(false);
+                                mainController.getMainWindow().toFront();
+                                mainController.getMainWindow().setFocusableWindowState(true);
+                            }
+                        });
+                    }
+                    return null;
+                }
+            });
+            return false;
+        } catch (AlreadyLockedException e) {
+            // Application already running.
+            return true;
+        }
+    }
 
-	public static void closeSplashScreen() {
-		if(SplashScreen.getSplashScreen() != null) {
-			SplashScreen.getSplashScreen().close();
-		}		
-	}
-	
-	public static String getAppVersion() {
-		Properties properties = new Properties();
-		try {
-			InputStream pomProperties = Jeboorker.class.getClass().getResourceAsStream("/META-INF/maven/org.rr/jeboorker-main/pom.properties");
-			if(pomProperties != null) {
-				properties.load(pomProperties);
-				return properties.getProperty("version");
-			}
-		} catch (IOException e) {
-			LoggerFactory.log(Level.SEVERE, Jeboorker.class, "Failed to load version.");
-		}
-		return "UNKNOWN"; // fallback version
-	}
+    public static void closeSplashScreen() {
+        if (SplashScreen.getSplashScreen() != null) {
+            SplashScreen.getSplashScreen().close();
+        }
+    }
 
-	/**
-	 * Dumps the application version and the vm start parameters to the log
-	 */
-	private static void logSystemInfo() {
-		RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
-		List<String> args = bean.getInputArguments();
-		String argsString = ListUtils.join(args, " ");
+    public static String getAppVersion() {
+        Properties properties = new Properties();
+        try {
+            URL resource = Jeboorker.class.getResource("/META-INF/maven/org.rr/jeboorker-main/pom.properties");
+            if (resource != null) {
+                try (InputStream pomProperties = resource.openStream()) {
+                    properties.load(pomProperties);
+                    return properties.getProperty("version");
+                }
+            } else {
+                LoggerFactory.log(Level.SEVERE, Jeboorker.class, "No version file found.");
+            }
+        } catch (IOException e) {
+            LoggerFactory.log(Level.SEVERE, Jeboorker.class, "Failed to load version.");
+        }
+        return "UNKNOWN"; // fallback version
+    }
 
-		LoggerFactory.getLogger().info("Jeboorker " + getAppVersion() + " started with " + argsString);
+    /**
+     * Dumps the application version and the vm start parameters to the log
+     */
+    private static void logSystemInfo() {
+        RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+        List<String> args = bean.getInputArguments();
+        String argsString = ListUtils.join(args, " ");
 
-		Properties props = System.getProperties();
-		Set<Object> keys = props.keySet();
-		for(Object key : keys) {
-			Object value = props.get(key);
-			LoggerFactory.getLogger().info(StringUtil.toString(key) + "=" + StringUtil.toString(value));
-		}
-	}
+        LoggerFactory.getLogger().info("Jeboorker " + getAppVersion() + " started with " + argsString);
 
-	/**
-	 * Get the application folder. Jeboorker must be configured that the app folder
-	 * is the current directory.
-	 */
-	public static String getAppFolder() {
-		return System.getProperties().getProperty("user.dir");
-	}
+        Properties props = System.getProperties();
+        Set<Object> keys = props.keySet();
+        for (Object key : keys) {
+            Object value = props.get(key);
+            LoggerFactory.getLogger().info(StringUtil.toString(key) + "=" + StringUtil.toString(value));
+        }
+    }
 
-	private static class ApplicationThreadFactory implements ThreadFactory {
+    /**
+     * Get the application folder. Jeboorker must be configured that the app folder
+     * is the current directory.
+     */
+    public static String getAppFolder() {
+        return System.getProperties().getProperty("user.dir");
+    }
 
-		private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private static class ApplicationThreadFactory implements ThreadFactory {
 
-		private final ThreadGroup group;
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
 
-		private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
 
-		private final String namePrefix;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
 
-		ApplicationThreadFactory() {
-			SecurityManager s = System.getSecurityManager();
-			group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-			namePrefix = "pool-" + poolNumber.getAndIncrement() + "-thread-";
-		}
+        private final String namePrefix;
 
-		public Thread newThread(final Runnable r) {
-			Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
-			if (t.isDaemon()) {
-				t.setDaemon(false);
-			}
-			if (t.getPriority() != Thread.NORM_PRIORITY) {
-				t.setPriority(Thread.NORM_PRIORITY);
-			}
-			return t;
-		}
-	}
+        ApplicationThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+            namePrefix = "pool-" + poolNumber.getAndIncrement() + "-thread-";
+        }
+
+        public Thread newThread(final Runnable r) {
+            Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+            if (t.isDaemon()) {
+                t.setDaemon(false);
+            }
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+                t.setPriority(Thread.NORM_PRIORITY);
+            }
+            return t;
+        }
+    }
 
 }
